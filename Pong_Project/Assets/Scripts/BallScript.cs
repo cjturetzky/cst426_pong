469521b8f827f0ff064e450fe19aadc48779fdaf
Connor Turetzky <cturetzky@gmail.com>
2022-02-03 10:07:40 -0800

    Added Ball
    Ball changes direction and angle based on where on the paddle it hit. dampenAngle variable allows adjusting how intense the angle change is.

---
+++ Pong_Project/Assets/Scripts/BallScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+
+    public int speed = 1;
+    public int maxspeed = 20;
+    public float dampenAngle = 0.2f;
+    public Vector3 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+    void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.tag == "Paddle"){
+            speed *= -1;
+            direction.y = (collision.gameObject.transform.position.y - collision.contacts[0].point.y) * dampenAngle;
+
+            if(Mathf.Abs(speed) < maxspeed){
+                if(speed < 0){
+                    speed -= 1;
+                }
+                else{
+                    speed += 1;
+                    direction.y *= -1;
+                }
+            }
+            
+            Debug.Log("Point: " + collision.contacts[0].point.y);
+            Debug.Log("Paddle: " + collision.gameObject.transform.position.y);
+        }
+        else if(collision.gameObject.tag == "Wall"){
+            direction.y *= -1;
+        }
+    }
+}
